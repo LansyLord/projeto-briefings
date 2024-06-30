311ed5ebf76a9ba827f9e9371595d2f6d23fc9a5
LansyLord <lansylord@gmail.com>
2024-06-30 16:22:46 -0300

    Adicionando botão de mudança de estado da Briefing

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -12,99 +12,99 @@ import { ClienteService } from '../servico/cliente.service';
 export class PrincipalComponent {
 
   // Variável para visibilidade dos botões
-  btnCadastro:boolean = true;
+  btnCadastro: boolean = true;
 
   // Variável para visibilidade da tabela
-  tabela:boolean=true;
+  tabela: boolean = true;
 
   briefing = new Briefing();
 
   // JSON de Briefings
-  briefings:Briefing[] = [];
+  briefings: Briefing[] = [];
 
   // JSON de Clientes
-  clientes:Cliente[] = [];
+  clientes: Cliente[] = [];
 
   // Construtor
   constructor(
-    private servicoBriefing:BriefingService,
-    private servicoCliente:ClienteService){}
+    private servicoBriefing: BriefingService,
+    private servicoCliente: ClienteService) { }
 
   // Método de seleção de briefings
-  selecionarBriefings():void{
+  selecionarBriefings(): void {
     this.servicoBriefing.selecionar()
-    .subscribe(retorno => this.briefings = retorno);
+      .subscribe(retorno => this.briefings = retorno);
   }
 
   // Método de registro de briefings
-  registrarBriefing():void{
+  registrarBriefing(): void {
     this.servicoBriefing.registrar(this.briefing)
-    .subscribe(retorno => {
-      // Registrar briefing
-      this.briefings.push(retorno);
+      .subscribe(retorno => {
+        // Registrar briefing
+        this.briefings.push(retorno);
 
-      // Limpar formulário
-      this.briefing = new Briefing();
+        // Limpar formulário
+        this.briefing = new Briefing();
 
-      // Mensagem
-      alert("Briefing cadastrada com sucesso");
+        // Mensagem
+        alert("Briefing cadastrada com sucesso");
 
-    });
+      });
   }
 
   //Método para edição de briefings
-  editarBriefing():void{
+  editarBriefing(): void {
     this.servicoBriefing.editar(this.briefing)
-    .subscribe(retorno => {
+      .subscribe(retorno => {
 
-      //Obter posição no vetor onde está a briefing
-      let posicao = this.briefings.findIndex(obj => {
-        return obj.id == retorno.id;
+        //Obter posição no vetor onde está a briefing
+        let posicao = this.briefings.findIndex(obj => {
+          return obj.id == retorno.id;
+        });
+
+        this.briefings[posicao] = retorno;
+
+        // Limpar formulário
+        this.briefing = new Briefing();
+
+        this.btnCadastro = true;
+
+        this.tabela = true;
+
+        alert("As alterações da briefing foram salvas");
+
       });
-
-      this.briefings[posicao] = retorno;
-
-      // Limpar formulário
-      this.briefing = new Briefing();
-
-      this.btnCadastro = true;
-
-      this.tabela = true;
-
-      alert("As alterações da briefing foram salvas");
-
-    });
   }
 
   // Método para remoção de briefings
-  removerBriefing(index:number):void{
+  removerBriefing(index: number): void {
 
     this.briefing = this.briefings[index]
 
-    this.servicoBriefing.remover(this.briefing.id)
-    .subscribe(retorno => {
+    const confirmation = confirm("Tem certeza que deseja apagar esta briefing?");
 
-      // Remover briefing do vetor
-      this.briefings.splice(this.briefing.id, 1);
+    if(confirmation){
+      this.servicoBriefing.remover(this.briefing.id)
+      .subscribe(retorno => {
 
-      // Limpar formulário
-      this.briefing = new Briefing();
+        // Remover briefing do vetor
+        this.briefings.splice(this.briefing.id, 1);
 
-      this.selecionarBriefings();
-
-      alert("Briefing removida com sucesso");
-
-    });
+        this.selecionarBriefings();
+      });
+    }
+    // Limpar formulário
+    this.briefing = new Briefing();
   }
 
   //Método de seleção de clientes
-  selecionarClientes():void{
+  selecionarClientes(): void {
     this.servicoCliente.selecionar()
-    .subscribe(retorno => this.clientes = retorno);
+      .subscribe(retorno => this.clientes = retorno);
   }
 
   // Método para selecionar uma briefing específica
-  selecionarBriefing(index:any){
+  selecionarBriefing(index: any) {
 
     this.briefing = this.briefings[index];
 
@@ -117,8 +117,37 @@ export class PrincipalComponent {
 
   }
 
+  // Método para mudar o estado da briefing
+  mudarEstadoBriefing(index: number) {
+    // Alternar o estado da briefing entre "APPROVED", "NEGOTIATION", e "FINISHED"
+    this.briefing = this.briefings[index]
+
+    const clienteEncontrado = this.clientes.find(c => c.name === this.briefing.clienteName);
+    this.briefing.cliente = clienteEncontrado ? clienteEncontrado : new Cliente();
+
+    switch (this.briefing.status) {
+      case 'APPROVED':
+        this.briefing.status = 'NEGOTIATION';
+        break;
+      case 'NEGOTIATION':
+        this.briefing.status = 'FINISHED';
+        break;
+      case 'FINISHED':
+        this.briefing.status = 'APPROVED';
+        break;
+      default:
+        this.briefing.status = 'APPROVED'; // Estado padrão
+    }
+
+    // Atualizar a briefing no servidor
+    this.servicoBriefing.editar(this.briefing)
+      .subscribe(() => {});
+
+    this.briefing = new Briefing();
+  }
+
   // Método para cancelar edição
-  cancelar():void{
+  cancelar(): void {
 
     this.briefing = new Briefing();
 
@@ -136,7 +165,7 @@ export class PrincipalComponent {
   }
 
   //Método de inicialização
-  ngOnInit(){
+  ngOnInit() {
     this.selecionarBriefings();
     this.selecionarClientes();
   }
